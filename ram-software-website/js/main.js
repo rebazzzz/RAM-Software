21040dd44cb6f5f205940d2aea9dd4b1f866a863
rebazzzz <rebazrawoof8@gmail.com>
2026-02-17 12:28:25 +0100

    updated the hero page and the header

--- ram-software-website/js/main.js
+++ ram-software-website/js/main.js
@@ -30,6 +30,8 @@ const ComponentLoader = {
 
     headerContainer.innerHTML = `
       <header class="border-b border-gray-200 dark:border-gray-800 bg-white/80 dark:bg-[#101722]/80 backdrop-blur-md sticky top-0 z-50">
+
+
         <div class="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
           <a href="index.html" class="logo">
             <div class="logo-icon">
